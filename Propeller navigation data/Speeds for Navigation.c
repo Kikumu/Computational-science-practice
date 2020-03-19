a2a9461cf694e1306f2b3bf68cd26d6164730af5
Ten5ei <scowty.sk@outlook.com>
2020-03-19 20:42:47 +0000

    left, right,360

--- Propeller navigation data/Speeds for Navigation.c
+++ Propeller navigation data/Speeds for Navigation.c
@@ -20,11 +20,26 @@ int var = 5;
  while (var < 7){
 
 drive_speed(64, 64);                       // Forward 64 tps for 2 s
-pause(2000);
+pause(1000);
+
+//turns 360
 drive_speed(0, 0);
-drive_speed(26, 0);                        // Turn 26 tps for 1 s
+drive_speed(45, 0);                        // Turn 26 tps for 1 s
 pause(1000);
 drive_speed(0, 0);
+drive_speed(0, -45); 
+
+//turns right
+drive_speed(0, 0);
+drive_speed(45, 0);                        // Turn 26 tps for 1 s
+pause(1000);
+
+//turns left
+drive_speed(0, 0);
+drive_speed(0, 45);                        // Turn 26 tps for 1 s
+pause(1000);
+
+//forward
 drive_speed(64, 64);                     // Forward 128 tps for 1 s
 pause(1000);
  }    
