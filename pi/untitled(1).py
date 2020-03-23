56b6abc52b8ae378bac675b8ae75fb302cdfa482
Ten5ei <scowty.sk@outlook.com>
2020-03-23 12:38:52 +0000

    steering skeleton applied

--- pi/untitled(1).py
+++ pi/untitled(1).py
@@ -19,46 +19,6 @@ def acquire_slope(var1,var2,var3,var4):
 	grad = (var2-var4)/(var1 - var3)
 	return grad
 	
-#remember has to be in the form of a numpy array
-def vct_sub(a,b):
-	val=b-a
-	return val
-	
-#remember has to be in the form of a numpy array
-def cross_vct(a,b):
-	res = np.cross(a, b)
-	magnitude = res[0]**2 + res[1]**2 + res[2]**2
-	return res,magnitude
-
-def  distance_between_skew_lines(var1,var2,var3,var4,var5,var6,var7,var8):
-	v1 = np.array([var1,var2,0])
-	v2 = np.array([var3,var4,0])
-	v3 = np.array([var5,var6,0])
-	v4 = np.array([var7,var8,0])
-	sub_res1 = vct_sub(v1,v2)
-	sub_res2 = vct_sub(v3,v4)
-	crs_product,magnitude = cross_vct(sub_res1,sub_res2)
-	a = sub_res1[0]
-	b = sub_res2[0]
-	c = crs_product[0]
-	d = sub_res1[1]
-	e = sub_res2[1]
-	f = crs_product[1]
-	g = sub_res1[2]
-	h = sub_res2[2]
-	i = crs_product[2]
-	a = np.array([[a,-b,-c],[d,-e,-f],[g,-h,-i]])
-	b = np.array([(var5-var1),(var6-var2),(-0 + 0)])
-	coeff_array = np.linalg.solve(a,b)
-	constant = coeff_array[2]
-	resultant = constant*magnitude
-	#d_1  = k(magnitude)
-	return resultant
-	
-def subtract_x_axis(var2,var4,var):
-	result = var4-var2
-	return result,var
-	
 def hsv_color_space(image):
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     l_h = cv2.getTrackbarPos("L-H","Trackbar")
@@ -70,7 +30,7 @@ def hsv_color_space(image):
     lower_red = np.array([l_h,l_s,l_v])
     upper_red = np.array([u_h,u_s,u_v])
     mask = cv2.inRange(hsv,lower_red,upper_red)
-    kernel = np.ones((5,5),np.uint8)
+    kernel = np.ones((10,10),np.uint8)
     mask = cv2.erode(mask,kernel)
     #contours
     A,contours,C = cv2.findContours(mask,cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -96,9 +56,6 @@ def hsv_color_space(image):
     return image,mask
 	
 
-def longest_line(var1,var2,var3,var4):
-	line_length =  ((var1 - var3)**2 + (var2 - var4)**2)**1/2
-	return line_length
 def region_of_interest(width,height):
     vertices = [(0,height),(0,300),(width/2,300),(width,300),(width,height)]
     return vertices
@@ -116,10 +73,10 @@ def draw_lines(img,mid_x,mid_y):
     hsv =cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
     #cv2.imshow("hsv",hsv)
     #cv2.waitKey()
-    low_yellow = np.array([119,45,0])
-    up_yellow = np.array([180,110,217])
+    low_yellow = np.array([142,89,180])
+    up_yellow = np.array([177,111,229])
     mask = cv2.inRange(hsv,low_yellow,up_yellow)
-    kernel = np.ones((6,6),np.uint8)
+    kernel = np.ones((10,10),np.uint8)
     mask = cv2.erode(mask,kernel)
     cv2.imshow("mask1",mask)
     #cv2.waitKey()
@@ -138,29 +95,48 @@ def draw_lines(img,mid_x,mid_y):
         gradient = acquire_slope(coord[0],coord[1],coord[2],coord[3]) #obtain gradient
         if(gradient > 0):
             img = cv2.circle(img,(int(p1),int(p2)),10,(0,255,0),-1)
-            print(p1,"right")
+            print(p1,"x_axis pos from right")
             print(mid_x-p1,"distance right")
         elif(gradient < 0):
             img = cv2.circle(img,(int(p1),int(p2)),10,(255,255,255),-1)
-            print(p1,"left")
+            print(p1,"x_axis pos from left")
             print(mid_x-p1,"distance left")
+#------------------------------------STEERING SYSTEM--------------------------------------------------------------##
+            if(p1 > 150 and (mid_x-p1)>63):
+                print("left")
+            elif(p1 > 150 and (mid_x-p1)<63):
+                print("right")
         else:
             print(gradient)	
     return img
-
+#-------------------------------------------------------------------------------------------------------------------##
+#------------------------------------STEERING CONTROL SKELETON------------------------------------------------------##
 def nothing(X):
 	pass
-	
 
-#template setup
+def adjust_left():
+    pass
+    
+def adjust_right():
+    pass
+    
+def steer_left_by_45():
+    pass
+    
+def forward():
+    pass
+def turn_round_360():
+    pass
+#--------------------------------------------------------------------------------------------------------------------##
+#--------------------------------------------template setup and trackbar---------------------------------------------##
 camera = PiCamera()
 camera.resolution = (640,480)
 camera.vflip = True
 camera.framerate = 20
 rawCapture = PiRGBArray(camera, size = (640,480))
 time.sleep(0.1)
-scrn_x = int(640*0.5)
-scrn_y = int((480*0.5) + 100)
+scrn_x = int(640*0.5)                         #set x position of centre dot
+scrn_y = int((480*0.5) + 100)                 #set y position of centre dot
 cv2.namedWindow("Trackbar")
 cv2.createTrackbar("L-H","Trackbar",0,180, nothing)
 cv2.createTrackbar("L-S","Trackbar",80,255, nothing)
@@ -169,20 +145,16 @@ cv2.createTrackbar("U-H","Trackbar",180,180, nothing)
 cv2.createTrackbar("U-S","Trackbar",255,255, nothing)
 cv2.createTrackbar("U-V","Trackbar",240,255, nothing)
 font = cv2.FONT_HERSHEY_COMPLEX
-
+#-------------------------------------------------------------------------------------------------------#
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     # grab the raw NumPy array representing the image, then initialize the timestamp
     # and occupied/unoccupied text
     image = frame.array
-    #---------------------------------SHAPE-----------------------------------------------
-    
-    #---------------------------------LINES--------------------------
-    #image = draw_lines(image,scrn_x,scrn_y)
+    #---------------------------------LINES----------------------------------------------------------------------
     vertices = region_of_interest(640,480) # create roi skeleton
     cropped = ROI_mask(image,np.array([vertices],np.int32),) #create cropped image usiing roi
     cropped = draw_lines(cropped,scrn_x,scrn_y) #draw lines on cropped image and pass as new image to display
-    
-    
+    #---------------------------------SHAPE-----------------------------------------------------------------------
     image,mask = hsv_color_space(image) # shape
     cv2.imshow("Frame", cropped)
     cv2.imshow("Mask",mask)
