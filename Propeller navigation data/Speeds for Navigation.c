9502e31ecc0e3cc63d76b1b51314f524a1295bbd
Ten5ei <scowty.sk@outlook.com>
2020-03-23 23:27:47 +0000

    ***

--- Propeller navigation data/Speeds for Navigation.c
+++ Propeller navigation data/Speeds for Navigation.c
@@ -30,41 +30,40 @@ v2 = adc_volts(2);  //right
 v1 = adc_volts(1);  //turn around
 v0 = adc_volts(0);  //forward
 
-//if(v0 > 2.5){
+if(v1 > 3.0){
   //forward
-  //drive_speed(64, 64);                       // Forward 64 tps for 2 s
-  //pause(1000);
-//  }
-if(v1 > 3.8){
+  drive_speed(0, 0); 
+  drive_speed(25, 25);  
+  drive_speed(0, 0);                     // Forward 64 tps for 2 s
+ // pause(1000);
+}
+//if(v0 > 3.8){
   //turns 360
-  drive_speed(0, 0);
-  drive_speed(45, 0);                        // Turn 26 tps for 1 s
-  pause(1000);
-  drive_speed(0, 0);
-  drive_speed(0, -45); 
-  drive_speed(0, 0);
-  }
- if(v2 > 3.8){
+ // drive_speed(0, 0);
+ // drive_speed(45, 0);                        // Turn 26 tps for 1 s
+ // pause(1000);
+//  drive_speed(0, 0);
+//  drive_speed(0, -45); 
+//  drive_speed(0, 0);
+//}
+else if(v2 > 3.8){
    //turns right at 90
-   drive_speed(0, 0);
-  //drive_speed(45, 0);                        // Turn 26 tps for 1 s
-   drive_speed(15, 0);
+   drive_speed(0, 0);                       // Turn 26 tps for 1 s
+   drive_speed(35, 0);
    drive_speed(0, 0);
   pause(1000);
  } 
-if(v3 > 3.8){
+else if(v3 > 3.8){
    //turns left at 90
    drive_speed(0, 0);
-   //drive_speed(0, 45);                        // Turn 26 tps for 1 s
-   drive_speed(0, 15);
+   drive_speed(0, 35);
    drive_speed(0, 0);
    pause(1000);
-  
   //---------------remaining code is to turn 90 so that it can keep 'one hand on wall'
 }    
 //drive_getTicks(&distLeft[1], &distRight[1]);
 //print("distLeft[1] = %d, distRight[1] = %d\n", distLeft[1], distRight[1]);
-//print("A/D3 = %f V%c\n", v3, CLREOL);     // Display volts
+//print("A/D3 = %f V%c\n", v1, CLREOL);     // Display volts
 // pause(1000);
  }    
 }
