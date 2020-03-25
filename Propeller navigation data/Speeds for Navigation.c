bf787b12a19ea0dd9ef13f0da7a692f00881afcb
Ten5ei <scowty.sk@outlook.com>
2020-03-25 19:50:13 +0000

    speeds adjustment

--- Propeller navigation data/Speeds for Navigation.c
+++ Propeller navigation data/Speeds for Navigation.c
@@ -23,6 +23,7 @@ int var = 5;
 float v1,v2,v3,v0;
 drive_getTicks(&distLeft[0], &distRight[0]);
 print("distLeft[0] = %d, distRight[0] = %d\n", distLeft[0], distRight[0]);
+int count = 0;
 //--------------------------------------loop-----------------------------------------------//
  while (var < 7){
 v3 = adc_volts(3);  //left 26
@@ -30,7 +31,9 @@ v2 = adc_volts(2);  //right 20
 v1 = adc_volts(1);  //turn around 16
 v0 = adc_volts(0);  //forward
 
-if(v1 > 3.0){ //16
+if (count > 100)
+{
+  if(v1 > 3.0){ //16
   //forward
   //drive_speed(0, 0); 
   drive_speed(25, 25);  
@@ -48,17 +51,20 @@ if(v1 > 3.0){ //16
 //}
 else if(v2 > 3.0){ //20
    //turns right at 90
-   drive_speed(0, 0);                       // Turn 26 tps for 1 s
+   //drive_speed(0, 0);                       // Turn 26 tps for 1 s
    drive_speed(25, 0);
-   drive_speed(0, 0);
+   drive_speed(25, 25);
+   //drive_speed(0, 0);
  } 
 else if(v3 > 3.0){ //26
    //turns left at 90
-   drive_speed(0, 0);
+   //drive_speed(0, 0);
    drive_speed(0, 25);
-   drive_speed(0, 0);
+   drive_speed(25, 25);
   //---------------remaining code is to turn 90 so that it can keep 'one hand on wall'
-}    
+} 
+  }
+count++;   
 //drive_getTicks(&distLeft[1], &distRight[1]);
 //print("distLeft[1] = %d, distRight[1] = %d\n", distLeft[1], distRight[1]);
 //print("A/D3 = %f V%c\n", v3, CLREOL);     // Display volts
