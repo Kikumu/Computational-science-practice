df34c9f28c8d57a54d8b1db9894530863e904213
Ten5ei <scowty.sk@outlook.com>
2020-04-14 23:37:18 +0100

    **

--- Propeller navigation data/Speeds for Navigation.c
+++ Propeller navigation data/Speeds for Navigation.c
@@ -12,6 +12,7 @@
 #include "abdrive.h"                          // abdrive library
 #include "adcDCpropab.h" 
 
+
 int distLeft[4], distRight[4];
 int main()                   
 {
@@ -22,19 +23,19 @@ int v1,v2,v3,v0,turn_around,status;
 drive_getTicks(&distLeft[0], &distRight[0]);
 print("distLeft[0] = %d, distRight[0] = %d\n", distLeft[0], distRight[0]);
 int count = 0;
-//--------------------------------------loop-----------------------------------------------//
- while (var < 7){
-//v3 = adc_volts(3);  //left 26
-//v2 = adc_volts(2);  //right 20
-//v1 = adc_volts(1);  //forward 16
-//v0 = adc_volts(0);  //turn around 21
-turn_around = input(10);
 v1 = input(1);
 v2 = input(0);
 v3 = input(2);
+//--------------------------------------loop-----------------------------------------------//
+ while (var < 7){
+turn_around = input(10);
+v1 = input(1); //16
+v2 = input(0); //20
+v3 = input(2); //26
 
+print("A/D3 = %f V%c\n", v3, CLREOL);      // Display volts
 //forward(16)
-if(v1 ==0){
+if(v1 == 0){
   status = 0;
   drive_speed(0, 0);
   }
@@ -50,8 +51,8 @@ else if(turn_around==1){
 //left(20)
 else if(v2==1){drive_speed(25, 0);}  
  //right(26)
-else if(v1==1){drive_speed(0, 25);}  
-//---------------remaining code is to turn 90 so that it can keep 'one hand on wall'
-//print("A/D3 = %f V%c\n", turn_around, CLREOL);     // Display volts
+else if(v3==1){drive_speed(0, 25);}  
+//print("A/D3 = %f V%c\n", v1, CLREOL);     // Display volts
+pause(1000);
  }    
 }
